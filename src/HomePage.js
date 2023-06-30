5a480032bcafe084d68d65d30ad56d2817aa8970
nicomilette <98992077+nicomilette@users.noreply.github.com>
2023-06-30 12:15:32 -0400

    Task list + formatting

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,16 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css';
 
+
 function HomePage() {
+
   const [selectedView, setSelectedView] = useState('calendar');
+  const [tasks, setTasks] = useState([]);
+  const [showNewTaskPopup, setShowNewTaskPopup] = useState(false);
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   const handleViewChange = (viewType) => {
     setSelectedView(viewType);
   };
 
   const handleNewTaskClick = () => {
-    console.log('Create New Task clicked');
-    // Add your logic for creating a new task here
+    setShowNewTaskPopup(true);
+  };
+
+  const handleSaveNewTask = () => {
+    if (title && details) {
+      const newTask = { title, details };
+      setTasks([...tasks, newTask]);
+      setTitle('');
+      setDetails('');
+    }
+    setShowNewTaskPopup(false);
   };
 
   return (
@@ -31,8 +58,63 @@ function HomePage() {
         </button>
       </div>
       <button className="new-task-button" onClick={handleNewTaskClick}>
-        Create New Task
+            Create New Task
+          </button>
+          {selectedView === 'taskList' && (
+  <>
+    <h2>Tasks:</h2> {/* Move the "Tasks" heading outside the task-list div */}
+    <div className="task-list">
+      {tasks && tasks.length > 0 ? (
+        tasks.map((task, index) => (
+          <div key={index} className="task-item">
+            <h3>{task.title}</h3>
+            <p>{task.details}</p>
+          </div>
+        ))
+      ) : (
+        <p>No tasks found.</p>
+      )}
+    </div>
+  </>
+)}
+
+
+      {showNewTaskPopup && (
+  <div className="new-task-popup">
+    <h2 className="new-task-popup-title">New Task</h2>
+    {/* Input fields for task information */}
+    <input
+            type="text"
+            placeholder="Task Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Task Details"
+            value={details}
+            onChange={(e) => setDetails(e.target.value)}
+          />
+    {/* Date and Time picker can be added here */}
+    {/* Save and Cancel buttons */}
+    <div className="new-task-popup-buttons">
+      <button className="save-task-button" onClick={() => handleSaveNewTask()}>
+        Save
       </button>
+      <button className="cancel-task-button" onClick={() => setShowNewTaskPopup(false)}>
+        Cancel
+      </button>
+    </div>
+  </div>
+)}
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
