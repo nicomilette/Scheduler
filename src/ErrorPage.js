4c3b6ab679dd7871f170eb83f3039f267260058c
nicomilette <98992077+nicomilette@users.noreply.github.com>
2023-06-30 12:41:02 -0400

    Error handling fix

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,16 +1,19 @@
 import { useRouteError } from "react-router-dom";
 import "./ErrorPage.css"
+
+
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="ErrorPage">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className="error-page">
+      <div className="error-box">
+        <h1 className="error-title">Oops, an error occurred!</h1>
+        <p className="error-message"><i>{error.statusText || error.message}</i></p>
+        <p className="error-description">Please try again later.</p>
+      </div>
     </div>
   );
 }
+
