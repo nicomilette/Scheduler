3cf4832aff03d75d255aa4cbb18a2e81dd880d1a
nicomilette <98992077+nicomilette@users.noreply.github.com>
2023-06-29 21:02:48 -0400

    9:02pm 6/29/23

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import WelcomePage from './WelcomePage';
+import Routing from './Routing';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <WelcomePage />
-        <div className="content">
-          <Routes>
-            <Route path="/">
-              <WelcomePage />
-            </Route>
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <div>
+      <Routing />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(
-  <React.StrictMode>
+
+const rootElement = document.getElementById('root')
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
 );
 
 
