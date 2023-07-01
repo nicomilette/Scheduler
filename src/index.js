6cbe2401d5fe46227f21b60e707f4899891225d9
nicomilette <98992077+nicomilette@users.noreply.github.com>
2023-07-01 19:27:06 -0400

    undo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 /* existing imports */
 import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { HashRouter, RouterProvider } from "react-router-dom";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import HomePage from "./HomePage";
 import WelcomePage from "./WelcomePage";
 import ErrorPage from "./ErrorPage";
 
-const routes = [
+const router = createBrowserRouter([
   {
     path: "/",
     element: <WelcomePage />,
@@ -21,16 +24,14 @@ const routes = [
     path: "/welcomepage",
     element: <WelcomePage />,
   },
-];
+]);
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <RouterProvider routes={routes} />
-    </HashRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
+
 
 /*
 
