8838c1a6f5df49e0b57156b7a87569f9f3d326e7
nicomilette <98992077+nicomilette@users.noreply.github.com>
2023-07-06 22:16:16 -0400

    error element

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import WelcomePage from './WelcomePage';
 import HomePage from './HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProtectedRoute from './ProtectedRoute';
+import ErrorPage from './ErrorPage';
 
 
 function App() {
@@ -10,11 +11,11 @@ function App() {
     <div>
       <Router>
           <Routes>
-            
-            <Route element={<WelcomePage/>} path="/"/>
+            <Route element={<WelcomePage/>} path="/" errorElement={<ErrorPage/>} />
             <Route element={<ProtectedRoute />}>
                 <Route element={<HomePage/>} path="/homepage" exact/>
             </Route>
+            
           </Routes>
       </Router>
     </div>
